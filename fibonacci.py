73e9c4056069fb8939bf7aae062bfc195964abc7
jczambon <zezinho158@gmail.com>
2024-05-14 13:01:22 -0300

    parte conceitual

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,9 @@
 def fib_linear(num):
+    #verifica se é maior que 0
     if num < 0:
         return 'O número precisa ser maior ou igual a 0'
     seq_fib = [0,1]
+    #vai adicionando até chegar no num
     for i in range(2, num+1):
         seq_fib.append(seq_fib[i-1] + seq_fib[i-2])
     return seq_fib[num]
@@ -12,8 +14,10 @@ def fib_recursivo(num):
     # Caso para fib(0) e fib(1)
     elif num <= 1:
         return num
+    #recursão ate chegar no caso inicial
     else:
         return fib_recursivo(num-1) + fib_recursivo(num-2)
 
+#exemplos
 print(fib_linear(6))
 print(fib_recursivo(1))
--- primo.py
+++ primo.py
@@ -1,10 +1,14 @@
 def primo_recursivo(num, i=2):
+    #verifica se numero é maior que 1
     if num < 2:
         return 'O número precisa ser maior que 1'
-    if num <= 2:
+    #verifica se é o final para encerrar
+    if num == 2:
         return [num]
+    #garante que é primo
     elif num == i:
         return primo_recursivo(num-1) + [num]
+    #não é primo
     elif num % i == 0:
         return primo_recursivo(num-1)
     else:
@@ -16,6 +20,7 @@ def primo_linear(num):
     lista_primos = []
     for num in range(2, num+1):
         primo = True
+        #utiliza raiz quadrada do num
         for i in range(2, int(num**0.5) + 1):
             if num % i == 0:
                 primo = False
@@ -24,6 +29,7 @@ def primo_linear(num):
             lista_primos.append(num)
     return lista_primos
 
+#exemplos
 print(primo_recursivo(20))
 print(primo_linear(20))
     
